5e2bf2cc158bcf20de1c1652d54b5fc62f26f516
orazioc17 <oraziocappadonna1999@gmail.com>
2021-06-25 14:46:16 -0400

    Usando la manera del profesor

---
+++ cuponesplatzi.js
@@ -0,0 +1,18 @@
+const cupones = [
+    {
+        nombre: 'namepippo_es_marico',
+        descuento: 0.15
+    },
+    {
+        nombre: 'pao_es_ladilla',
+        descuento: 0.20
+    },
+    {
+        nombre: 'salva_es_gay',
+        descuento: 0.30
+    },
+    {
+        name: 'paula_te_amo',
+        descuento: 0.95
+    }
+];
