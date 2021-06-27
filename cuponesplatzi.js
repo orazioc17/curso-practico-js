0f851d1e7bb11078d0306df1a5e2447784569b11
orazioc17 <oraziocappadonna1999@gmail.com>
2021-06-27 10:21:38 -0400

    Finished cupones patzi

--- cuponesplatzi.js
+++ cuponesplatzi.js
@@ -1,18 +1,44 @@
-const cupones = [
+function calcularDescuento(precio, descuento) {
+    let montoDescuento = precio * (descuento/100);
+    return precio - montoDescuento;
+}
+
+const coupons = [
     {
-        nombre: 'namepippo_es_marico',
-        descuento: 0.15
+        name: "Hola mundo",
+        discount: 15,
     },
     {
-        nombre: 'pao_es_ladilla',
-        descuento: 0.20
+        name: "foo",
+        discount: 30,
     },
     {
-        nombre: 'salva_es_gay',
-        descuento: 0.30
+        name: "bar",
+        discount: 25,
     },
-    {
-        name: 'paula_te_amo',
-        descuento: 0.95
-    }
 ];
+
+function calcular() {
+    const inputPrice = document.getElementById('monto');
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById('cupon');
+    const couponValue = inputCoupon.value;
+    
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + "no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+  
+        const precioConDescuento = calcularDescuento(priceValue, descuento);
+  
+        const resultP = document.getElementById("resultado");
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
+}
